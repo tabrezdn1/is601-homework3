45edc8b3dadba101fabab722d6c903010d911be5
Shaik Tabrez <37013844+tabrezdn1@users.noreply.github.com>
2024-02-13 18:49:27 -0500

    Advance calculator with parameterized test data

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -9,10 +9,10 @@ def test_subtraction():
     '''Test that addition function works '''    
     assert Calculator.subtract(2,2) == 0
 
-def test_multiply():
-    '''Test that addition function works '''    
-    assert Calculator.multiply(2,2) == 4
-
 def test_divide():
-    '''Test that addition function works '''    
+    '''Test that division function works '''    
     assert Calculator.divide(2,2) == 1
+
+def test_multiply():
+    '''Test that multiply function works '''    
+    assert Calculator.multiply(2,2) == 4
